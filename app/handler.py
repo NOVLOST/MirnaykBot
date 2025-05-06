74e3cb9809fc6d283295bfd3838d02e6af346554
NOVLOST <159267939+NOVLOST@users.noreply.github.com>
2025-05-06 13:03:46 +0400

    Add files via upload

---
+++ app/handler.py
@@ -0,0 +1,41 @@
+from aiogram import F, Router,Bot
+from aiogram.fsm.context import FSMContext
+from aiogram.types import Message,CallbackQuery,ReplyKeyboardRemove
+from aiogram.filters import CommandStart
+
+import app.keyboards as kb
+import classes.client_class as cl
+
+
+router = Router()
+bot = Bot(token='7662881278:AAGGFZ3tj7rC4DxOvfXlp51Qho5aeWC4uJ0')
+
+
+@router.message(CommandStart())
+async def cmd_start(message: Message):
+    await message.answer("Здравствуйте!мои команды:\n1./sendandpin - команда для закрепления и отправки  сообщения в чат  ")
+
+@router.message(F.text == "/sendandpin")
+async def cmd_sendandpin(message: Message,state: FSMContext):
+    await state.set_state(cl.client.send_and_pin_text)
+    await message.answer("отправте любое сообщение и я его закреплю в чате.")
+
+
+
+
+
+
+@router.message(cl.client.send_and_pin_text)
+async def sendandpin_mod(message: Message,state: FSMContext):
+    await state.update_data(send_and_pin_text = message.text)
+    data = await state.get_data()
+    sent_message = await bot.send_message(
+        chat_id="-1001603940184",
+        text=data["send_and_pin_text"],
+        parse_mode="HTML"
+    )
+
+    await bot.pin_chat_message(
+        chat_id= "-1001603940184",
+        message_id=sent_message.message_id
+    )
