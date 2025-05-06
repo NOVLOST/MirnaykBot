e12cc274fec35594d3f2adedf827f6e6c5b19e0e
NOVLOST <159267939+NOVLOST@users.noreply.github.com>
2025-05-06 13:02:42 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+import asyncio
+from aiogram import Dispatcher
+
+from app.handler import router,bot
+
+
+
+async def main():
+
+    dp = Dispatcher()
+    dp.include_router(router)
+    print("Бот включен")
+    await dp.start_polling(bot)
+
+if __name__ == '__main__':
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print('Бот выключен')
+
+"""функции бота :
+   1. ежедневный парсинг  подобных данных
+"Седмица 3-я по Пасхе. Глас 2.
+
+Поста нет. 
+Вмч. Георгия Победоносца (303). Иверской иконы Божией Матери (второе обретение списка иконы 2012).
+Мц. царицы Александры (314). Мчч. Анатолия и Протолеона(303). Прп. Софии (1974). 
+Сщмч. Иоанна пресвитера (1940).  конечно же сслыки на этих святых
+ https://days.pravoslavie.ru/Days/20250216.html или https://azbyka.ru/days/ 
+ 
+ 2.расписание богослужений чтобы постил и закреплелял
+ 3. возможность отправить боту сообщение которое он запостит и закрепит"""
