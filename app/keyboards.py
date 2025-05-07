824137a0a89018f00436c37e374c553b46817496
NOVLOST <159267939+NOVLOST@users.noreply.github.com>
2025-05-07 08:50:38 +0400

    Update keyboards.py

--- app/keyboards.py
+++ app/keyboards.py
@@ -2,4 +2,16 @@ from aiogram.types import (ReplyKeyboardMarkup, KeyboardButton,
                            InlineKeyboardButton,InlineKeyboardMarkup)
 
 
+start_keyboard = ReplyKeyboardMarkup(
+    keyboard=[
+        [
+            KeyboardButton(text="Расписание на богослужений на эту неделю 📝")
+
+        ],
+    ],
+    resize_keyboard=True,
+    input_field_placeholder="Что вас интересует?"
+)
+
+
 
