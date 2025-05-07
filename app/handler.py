21d607a2751c61c5b303ea80f3a8744865bb3b2e
NOVLOST <159267939+NOVLOST@users.noreply.github.com>
2025-05-07 08:50:22 +0400

    Update handler.py

--- app/handler.py
+++ app/handler.py
@@ -2,6 +2,8 @@ from aiogram import F, Router,Bot
 from aiogram.fsm.context import FSMContext
 from aiogram.types import Message,CallbackQuery,ReplyKeyboardRemove
 from aiogram.filters import CommandStart
+from aiogram.types import FSInputFile
+
 
 import app.keyboards as kb
 import classes.client_class as cl
@@ -13,16 +15,20 @@ bot = Bot(token='7662881278:AAGGFZ3tj7rC4DxOvfXlp51Qho5aeWC4uJ0')
 
 @router.message(CommandStart())
 async def cmd_start(message: Message):
-    await message.answer("Здравствуйте!мои команды:\n1./sendandpin - команда для закрепления и отправки  сообщения в чат  ")
+    await message.answer("Здравствуйте!мои команды:\n"
+                         "1./sendandpin - команда для закрепления и отправки  сообщения в чат\n"
+                         ,reply_markup=kb.start_keyboard)
+
+
+@router.message(F.text == "Расписание на богослужений на эту неделю 📝")
+async def cmd_timetable(message: Message):
+    photo = FSInputFile("timetable.jpg")
+    await bot.send_photo(chat_id="-1001603940184", photo=photo)
 
 @router.message(F.text == "/sendandpin")
 async def cmd_sendandpin(message: Message,state: FSMContext):
     await state.set_state(cl.client.send_and_pin_text)
     await message.answer("отправте любое сообщение и я его закреплю в чате.")
-
-
-
-
 
 
 @router.message(cl.client.send_and_pin_text)
